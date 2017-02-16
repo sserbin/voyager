5329d1288bc16be80838797e8fbb5f75298f1084
Kanit Wongsuphasawat <kanitw@gmail.com>
2017-02-15 16:54:14 -0800

    Add Redux Logger

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,16 @@
-import {createStore} from 'redux';
-import {} from 'redux-undo';
+import {createStore, applyMiddleware} from 'redux';
+import * as createLogger from 'redux-logger';
 
 import {DEFAULT_STATE} from '../models';
 import {rootReducer} from '../reducers';
 
+const logger = createLogger();
+
 export function configureStore(initialState = DEFAULT_STATE) {
-  const store = createStore(rootReducer, {past: [], present: initialState, future: []});
+  const store = createStore(
+    rootReducer,
+    {past: [], present: initialState, future: []},
+    applyMiddleware(logger)
+  );
   return store;
 }
